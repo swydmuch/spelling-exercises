e705a51577762d1178c6253e94952abf86a1e7b0
sylwester.wydmuch <sylwester.wydmuch@librus.pl>
2022-04-27 21:32:36 +0200

    Fixed select empty option

--- src/components/CharactersList.js
+++ src/components/CharactersList.js
@@ -4,6 +4,7 @@ export class CharactersList extends React.Component {
     render() {
         return (
             <select className="List" onChange={this.props.onChange}>
+                <option key="-1" value="-1"></option>
                 {this.props.data.map(
                     (characters, index) => <option key={index} value={characters.value} >{characters.label}</option>
                 )}
