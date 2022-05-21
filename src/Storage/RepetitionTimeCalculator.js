ec08671b3d37d38baa29f9b9f32f40c4f1d5bb1b
sylwester.wydmuch <sylwester.wydmuch@librus.pl>
2022-05-21 15:45:01 +0200

    Changed frequency of repetition lerned words
    Previously frequency was too small.
    Now first 10th repetitioin is in next day and rest is increment by
    1 day per repetion

--- src/Storage/RepetitionTimeCalculator.js
+++ src/Storage/RepetitionTimeCalculator.js
@@ -1,14 +1,19 @@
+import {LearningStorage} from "./LearningStorage";
+
 export class RepetitionTimeCalculator {
+
+    static ANSWERS_WITH_NEXT_DAY_REPETITION = 10;
+
     static countDaysFor(answerStreak) {
         if (answerStreak < 2) {
-            throw new Error('Repetition time is calculate from 2 correct answers');
+            throw new Error('Repetition time is calculate from ' + LearningStorage.ANSWER_FOR_LEARNED +  ' correct answers');
         }
 
-        if(answerStreak === 2) {
+        if(answerStreak <= RepetitionTimeCalculator.ANSWERS_WITH_NEXT_DAY_REPETITION) {
             return 1;
         }
 
-        return (RepetitionTimeCalculator.countDaysFor(answerStreak - 1) + (answerStreak - 2));
+        return (RepetitionTimeCalculator.countDaysFor(answerStreak - 1) + 1);
     }
 
     static countTimeFor(answerStreak) {
